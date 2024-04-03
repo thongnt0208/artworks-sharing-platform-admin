5a027d4823d6cd365732903ad80e2e0dd6b0b1fc
Dang Hoang Anh <82361430+danghoanganh36@users.noreply.github.com>
2024-04-03 11:50:44 +0700

    Anh - Update noti and services, add tabs

--- src/sections/moderate/moderate-left-nav.jsx
+++ src/sections/moderate/moderate-left-nav.jsx
@@ -1,23 +1,40 @@
 /* eslint-disable */
 // ----------------------------------------------------------------------
-import React from 'react'
+import React from 'react';
 
-import MessageItemTemplate from './moderate-message-item'
+import MessageItemTemplate from './moderate-message-item';
 import { DataScroller } from 'primereact/datascroller';
+import { Tab, Tabs } from '@mui/material';
+import { TabContext, TabList } from '@mui/lab';
+import { Box } from '@mui/system';
 
 export default function ModerateLeftNav(props) {
-  const { itemsList, selectingId, setSelectingId, setAccountId } = props;
+  const { tabValue, setTabValue, itemsList, selectingId, setSelectingId, setAccountId } = props;
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
   return (
-    <div className='max-h-full'>
-      <p className='text-cus-h2-bold m-0 mb-3'>Chưa duyệt</p>
+    <div className="max-h-full">
+      <h2 className="text-left m-0 mb-2 p-0">Danh sách tác phẩm</h2>
+      <TabContext value={tabValue}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Chưa duyệt" value="0" />
+            <Tab label="Chấp nhận" value="1" />
+            <Tab label="Từ chối" value="2" />
+          </TabList>
+        </Box>
+      </TabContext>
       <DataScroller
         value={itemsList}
-        emptyMessage='Không có tác phẩm nào cần duyệt' 
-        rows={8} 
+        emptyMessage="Không có tác phẩm nào cần duyệt"
+        rows={8}
         buffer={0.4}
-        inline 
+        inline
         lazy={true}
-        scrollHeight='80vh'
+        scrollHeight="80vh"
         itemTemplate={(item) => (
           <MessageItemTemplate
             key={item.id}
@@ -29,5 +46,5 @@ export default function ModerateLeftNav(props) {
         )}
       />
     </div>
-  )
+  );
 }
--- src/sections/moderate/view/moderate-view.jsx
+++ src/sections/moderate/view/moderate-view.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable */
 // ----------------------------------------------------------------------
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Toast } from 'primereact/toast';
 
 import ModerateLeftNav from '../moderate-left-nav';
 import ModerateContent from '../moderate-content';
@@ -10,13 +11,8 @@ import { downloadAssets, getArtworksData, updateArtworkState } from '../Service'
 import { getArtworkDetailData } from '../Service';
 
 export default function ModerateView() {
-  //TODO:
-  // - ITG Report artwork
-  // - ITG download assets artwork
-  // - Fix UI for ModerateLeftNav
-  // - Fix bug when accept artwork but not re-render ModerateContent & ModerateRightNav
-  // - Update set new pageNumber when scroll to bottom
-  // - Handle regex for note
+  const toast = useRef(null);
+  const [tabValue, setTabValue] = React.useState('0');
   const [refresh, setRefresh] = React.useState(false);
   const [selectingId, setSelectingId] = React.useState('');
   const [accountId, setAccountId] = React.useState('');
@@ -25,30 +21,42 @@ export default function ModerateView() {
   const [account, setAccount] = React.useState({});
   const [pageNumber, setPageNumber] = React.useState(1);
 
+  const showSuccess = (summary, detail) => {
+    toast.current.show({ severity: 'success', summary: summary, detail: detail, life: 3000 });
+  };
+
+  const showError = (summary, detail) => {
+    toast.current.show({ severity: 'error', summary: summary, detail: detail, life: 3000 });
+  };
+
   const handleModerateCallback = (state, note) => {
     if (selectingId === null) return;
     // Call API to update artwork state
     const response = updateArtworkState(selectingId, state, note);
-    if (response) {
-      console.log('Update artwork state successfully', response);
-      setRefresh(true);
-    }
-  };
-
-  const handleGetArtworksData = (pageNumber, pageSize) => {
-    const artworks = getArtworksData(pageNumber, pageSize);
-    artworks.then((data) => {
-      const updatedArtworks = data.map((artwork) => ({ ...artwork, isSeen: false }));
-      setArtworks(updatedArtworks);
-      if (selectingId === '' || accountId === '' || account === null) {
-        setSelectingId(updatedArtworks[0]?.id);
-        setAccountId(updatedArtworks[0]?.account.id);
-        setAccount(updatedArtworks[0]?.account);
+    response.then((data) => {
+      if (data.status === 204) {
+        showSuccess('Thành công', `Duyệt bài thành công: ${state === 1 ? 'Chấp nhận' : 'Từ chối'}`);
+        setRefresh(true);
+      } else {
+        showError('Thất bại', `Lỗi: ${data.status} - Duyệt bài thất bại`);
       }
     });
   };
 
+  const handleGetArtworksData = (state, pageNumber, pageSize) => {
+    const artworks = getArtworksData(state, pageNumber, pageSize);
+    artworks.then((data) => {
+      const updatedArtworks = data.map((artwork) => ({ ...artwork, isSeen: false }));
+      setArtworks(updatedArtworks);
+      setSelectingId(updatedArtworks[0]?.id);
+      setAccountId(updatedArtworks[0]?.account.id);
+      setAccount(updatedArtworks[0]?.account);
+    });
+    setRefresh(false);
+  };
+
   const handleDownloadAssets = (id) => {
+    if (id === null) return;
     downloadAssets(id)
       .then((link) => {
         window.open(link, '_blank');
@@ -65,9 +73,11 @@ export default function ModerateView() {
   }, [selectingId]);
 
   useEffect(() => {
-    handleGetArtworksData(pageNumber, 10);
-    setRefresh(false);
-  }, [refresh]);
+    setSelectingId('');
+    setAccountId('');
+    setAccount({});
+    handleGetArtworksData(tabValue, pageNumber, 50);
+  }, [refresh, tabValue]);
 
   useEffect(() => {
     const currentAccount = artworks.find((artwork) => artwork.id === selectingId)?.account;
@@ -76,8 +86,11 @@ export default function ModerateView() {
 
   return (
     <div className="grid" style={{ maxHeight: '80vh', padding: '20px 0' }}>
+      <Toast ref={toast} />
       <div className="left-nav col-3 max-h-full">
         <ModerateLeftNav
+          tabValue={tabValue}
+          setTabValue={setTabValue}
           itemsList={artworks}
           selectingId={selectingId}
           setSelectingId={setSelectingId}
